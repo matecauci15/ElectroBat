ab95f0b054a0b274610b9f72f85335624c3cc8a4
matecauci15 <matecaucino@gmail.com>
2025-08-20 11:42:09 -0300

    pascalcase

--- electrobat/src/App.tsx
+++ electrobat/src/App.tsx
@@ -1,17 +1,22 @@
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import Whatsappbutton  from './Components/Whatsappbutton';
-import DomicilioSections from './assets/cambioadomicilio';
-import CTAInmediataSection from './assets/ctainmediato';
-import { BatteryReplacementSection } from './Components/cambiobateria';
+import DomicilioSections from './assets/Cambioadomicilio';
+
 import { TestimonialsSection } from './assets/TestimonialsSection';
 import { Footer } from './assets/Footer';
 import TagManager from 'react-gtm-module';
 import VolquetesLanding from './pages/volqueteslanding'; 
 import Navbar from './assets/Navbar';
-import { AboutSection } from './assets/Aboutsection';
-import { Banner } from './assets/banner';
-import { BatteriesSection } from './assets/Batteriessection';
+// import { AboutSection } from './assets/Aboutsection';
+import { Banner } from './assets/Banner';
+
+import { BatteryReplacementSection } from './Components/Cambiobateria';
+import CTAInmediataSection from './assets/CTAInmediato';
+import { AboutSection } from './assets/AboutSection';
+import { BatteriesSection } from './assets/BatteriesSection';
+
+
 
 const tagManagerArgs = {
   gtmId: 'GTM-5B8CQNP'
@@ -33,13 +38,13 @@ function LandingPrincipal() {
           <TestimonialsSection />
         </section>
         <section id="about">
-          <AboutSection />
+          <AboutSection/>
         </section>
         <section id="cambiobateria">
           <BatteryReplacementSection />
         </section>
         <section id="batteries">
-          <BatteriesSection />
+          <BatteriesSection/>
         </section>
       </main>
       <Whatsappbutton />
@@ -53,7 +58,7 @@ function App() {
 <Routes>
   <Route path="/" element={<LandingPrincipal />} />
   <Route path="/volquetes" element={<VolquetesLanding />} />
-  <Route path="*" element={<LandingPrincipal />} /> {/* Redirige todo lo demás a landing */}
+  <Route path="*" element={<LandingPrincipal />} /> 
 </Routes>
 
   );
--- electrobat/src/pages/volqueteslanding.tsx
+++ electrobat/src/pages/volqueteslanding.tsx
@@ -1,18 +1,18 @@
 
-// import Navbar from '../assets/Navbar';
 import { Footer } from '../assets/Footer';
+// import Navbar from '../assets/Navbar';
 // Podés importar componentes existentes o nuevos personalizados
 // import { ContactSection } from '../assets/Contactsection';
 // import CTAInmediataSection from '../assets/volquetesctainmediato';
+// import VolquetesCTASection from '../Volquetes/volquetesctainmediato';
+// import { CTAFinalSimplevolquetes } from '../Volquetes/CTAfinalvolquetes';
 import { TestimonialsSection } from '../assets/TestimonialsSection';
 import Whatsappbutton from '../Components/Whatsappbutton';
 import { Navbarvolquetes } from '../Volquetes/Navbarvolquetes';
 import { Comofunciona } from '../Volquetes/comofunciona';
 import CTAInmediataSectionvolquetes from '../Volquetes/ctainmediatovolquetes';
-// import VolquetesCTASection from '../Volquetes/volquetesctainmediato';
 import { AboutSectionvolquetes } from '../Volquetes/Aboutsectionvolquetes';
 import { Cuandonecesitas } from '../Volquetes/cuandonecesitas';
-// import { CTAFinalSimplevolquetes } from '../Volquetes/CTAfinalvolquetes';
 import { VolquetesSection } from '../Volquetes/Volquetes';
 import TagManager from 'react-gtm-module';
 import { BannerVolquetes } from '../Volquetes/bannervolquetes';
